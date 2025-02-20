9b15a83e04691c9a2c4417d3a123d8e1789496d4
Lais Bandeira <lbms@cin.ufpe.br>
2025-02-20 16:35:56 -0300

    style(icon): Adiciona o icone

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,12 +8,17 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Página pessoal de Ibandeira' },
+      { hid: 'description', name: 'description', content: 'Página pessoal de lbandeira' },
     ],
     link: [
       {
         rel: 'stylesheet',
         href: 'https://fonts.googleapis.com/css2?family=Inria+Sans:wght@400;700&display=swap',
+      },
+      {
+        rel: 'icon',
+        type: 'image/svg+xml',
+        href: '~/static/minamaker.svg', // Caminho para o ícone na pasta static/
       },
     ],
   },
