3195ac70438764b5498ee611cc2492804e3e8243
lbandeira <lbms@cin.ufpe.br>
2025-07-29 22:16:13 -0300

    Deploy: static site to GitHub Pages

--- nuxt.config.ts
+++
@@ -1,8 +0,0 @@
-export default  defineNuxtConfig({
-  // Configurações do Nuxt
-  // Ou 'server' se for um projeto SSR
-  css: ["~/assets/css/tailwind.css"],
-
-  modules: ['@nuxtjs/tailwindcss'],
-  compatibilityDate: '2025-07-29',
-})
